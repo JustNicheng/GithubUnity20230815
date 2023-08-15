fcd6f2c900f41b29b312bbba1a71b19427c9254d
USER <USER@PC409019>
2023-08-15 13:18:02 +0800

    Update scene.cs

--- Assets/menu/menu.cs
+++ Assets/menu/menu.cs
@@ -4,7 +4,7 @@ namespace JSNi
 {
     public class menu : MonoBehaviour
     {
-
+        string name = "game";
     }
 }
 
---
+++ Assets/menu/scene.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+namespace JSNi { 
+
+public class scene : MonoBehaviour
+{
+}
+
+
+}
